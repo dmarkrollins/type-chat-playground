c507f6680a5decbdcb8c380cc09fae5308e8627e
Dave Rollins <drollins@sdcsol.com>
2023-07-29 15:01:42 -0400

    clean up

--- examples/events/dist/eventSchema.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.labStyle = exports.eventItemLengthMinutes = void 0;
-exports.eventItemLengthMinutes = [60, 90, 120];
-exports.labStyle = ["Hands On", "Video"];
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXZlbnRTY2hlbWEuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvZXZlbnRTY2hlbWEudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7O0FBT2EsUUFBQSxzQkFBc0IsR0FBRyxDQUFDLEVBQUUsRUFBRSxFQUFFLEVBQUUsR0FBRyxDQUFDLENBQUM7QUFvQnZDLFFBQUEsUUFBUSxHQUFHLENBQUMsVUFBVSxFQUFFLE9BQU8sQ0FBQyxDQUFDIn0=
--- examples/sentiment/dist/sentimentSchema.ts
+++
@@ -1,4 +0,0 @@
-export interface SentimentResponse {
-    /** The sentiment of the text. */
-    sentiment: "negative" | "neutral" | "positive";
-}
